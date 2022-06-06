9355ad67a724a5ed75b66770155f32996cb9c698
coderlch <2608073916@qq.com>
2022-06-06 16:53:55 +0800

    项目初始结构创建

---
+++ order/src/main/java/cn/coderlch/gulimall/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package cn.coderlch.gulimall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
